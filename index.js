66b327a9f75dc3f521ee622a52c3034dcdff9424
Selno <meitxyps385@gmail.com>
2021-03-08 22:27:40 +0900

    引数に与えたリスト内の整数をすべて掛け合わせる関数を定義しました

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-'use strict';
+"use strict"
 function add(numbers) {
     let result = 0;
-    for (let num of numbers) {
-        result = result + num;
+    for (let i of numbers) {
+        result += i;
     }
-    return result;
+    return result
 }
+function multi(numbers) {
+    let result = 1;
+    for (let i of numbers) {
+        result *= i;
+    }
+    return result
+}
+
 module.exports = {
-    add: add
+    add: add,
+    multi: multi
 };
